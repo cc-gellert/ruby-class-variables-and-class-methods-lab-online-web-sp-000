308ef905616cb88a0d7ef8b0e5eb33f4e008ea1a
Chloe Gell <gellert.chloe@gmail.com>
2020-01-31 02:04:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,26 @@ class Song
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
+    @@artists << artist 
     @genre = genre 
+    @@genres << genre 
     @@count += 1 
   end 
   @@count = 0 
   def self.count 
     @@count
   end 
-  @@genres = 0 
+  @@genres = [] 
   def self.genres
     @@genres 
+  end 
+  @@artists = [] 
+  def self.artists 
+    @@artists 
+  end
+  def self.genre_count
+    
+  end 
+  def self.artist_count
   end 
 end 
